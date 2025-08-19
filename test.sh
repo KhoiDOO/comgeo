fcc8c1b8527b20976154aa3ab3bf8655a10d2cfb
KhoiDOO <khoido8899@gmail.com>
2025-08-19 01:29:42 +0100

    Update pyproject.toml and test.sh for configuration and test script changes

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 # Vertex
-
 bash test_script/vertex/test_core.sh
 bash test_script/vertex/test_functional_triplets_check.sh
 
 # Vector
-
 bash test_script/vector/test_core.sh
 
 # Polygon
-
 bash test_script/polygon/test_core.sh
+bash test_script/polygon/test_functional.sh
+
+# Mesh
+bash test_script/mesh/test_core.sh
