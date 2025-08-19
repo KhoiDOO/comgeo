2a3d502ce780a5f398178b65e39c201ea1cea006
KhoiDOO <khoido8899@gmail.com>
2025-08-19 01:29:18 +0100

    Add comprehensive unittest for Face primitive and fix error handling in Face implementation

---
+++ comgeo/core/mesh/primitives/face.py
@@ -0,0 +1,105 @@
+from ...vertex import Vertex, Vertex2D, Vertex3D
+from ....functional.vertex.triplets.check import is_ccw
+from ....functional.polygon.center import get_center
+
+from ....decorator.error import not_instance, not_self_implemented, not_self_instance
+from ...utils.error import check_type, check_consistency
+
+
+class Face:
+    def __init__(self, vertex_ids: list[int], id: int = -1, visited: bool = False, max_num_vertices: int | None = None):
+        check_type(vertex_ids, list, "vertex_ids")
+        check_type(id, int, "id")
+        check_type(visited, bool, "visited")
+        check_type(max_num_vertices, int | None, "max_num_vertices")
+        self._vertex_ids = vertex_ids
+        self._id = id
+        self._visited = visited
+        self._center: Vertex2D | Vertex3D | None = None
+        self._area: float | None = None 
+        self._max_num_vertices = max_num_vertices
+
+        if max_num_vertices is not None:
+            assert len(vertex_ids) <= max_num_vertices, "Number of vertices in face exceeds max_num_vertices"
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @id.setter
+    @not_instance(int)
+    def id(self, id: int):
+        self._id = id
+    
+    @property
+    def visited(self):
+        return self._visited
+    
+    @visited.setter
+    @not_instance(bool)
+    def visited(self, visited: bool):
+        self._visited = visited
+    
+    @property
+    def vertex_ids(self):
+        return self._vertex_ids
+    
+    @vertex_ids.setter
+    @not_instance(list)
+    def vertex_ids(self, vertex_ids: list[int]):
+        check_consistency(vertex_ids, "vertex_ids")
+        if self._max_num_vertices is not None:
+            assert len(vertex_ids) <= self._max_num_vertices, "Number of vertices in face exceeds max_num_vertices"
+        self._vertex_ids = vertex_ids
+    
+    @property
+    def center(self, all_vertices: list[Vertex | Vertex2D | Vertex3D]):
+        self._center = get_center([all_vertices[i] for i in self._vertex_ids])
+        return self._center
+    
+    @center.setter
+    @not_instance(Vertex2D | Vertex3D)
+    def center(self, center: Vertex2D | Vertex3D):
+        self._center = center
+    
+    @property
+    @not_self_implemented
+    def area(self, all_vertices: list[Vertex | Vertex2D | Vertex3D]):
+        pass
+    
+    @area.setter
+    @not_instance(float)
+    @not_self_implemented
+    def area(self, area: float):
+        pass
+
+    @not_self_instance
+    def __eq__(self, other: 'Face') -> bool:
+        return self.id == other.id and self.visited == other.visited and self.vertex_ids == other.vertex_ids
+    
+    def __repr__(self):
+        return f"Face(id={self._id}, visited={self._visited}, \nvertex_ids={self._vertex_ids})"
+    
+    def __str__(self):
+        return f"Face(id={self._id}, visited={self._visited}, \nvertex_ids={self._vertex_ids})"
+    
+    def __getitem__(self, key: int) -> int:
+        return self._vertex_ids[key]
+    
+    def __setitem__(self, key: int, value: int):
+        self._vertex_ids[key] = value
+
+    def is_convex(self, all_vertices: list[Vertex | Vertex2D | Vertex3D]) -> bool:
+        vertices = [all_vertices[i] for i in self._vertex_ids]
+
+        if type(vertices[0]) in [Vertex, Vertex2D]:
+            raise NotImplementedError("is_convex not implemented for " + str(type(vertices[0])))
+        
+        for i in range(len(vertices)):
+            if not is_ccw(vertices[i], vertices[(i + 1) % len(vertices)], vertices[(i + 2) % len(vertices)]):
+                return False
+        return True
+
+    @not_self_implemented
+    def point_cloud_sampling(self, num_points: int, all_vertices: list[Vertex | Vertex2D | Vertex3D]) -> list[Vertex | Vertex2D | Vertex3D]:
+        pass
---
+++ test/mesh/primitives/test_face.py
@@ -0,0 +1,88 @@
+import unittest
+from comgeo.core.mesh.primitives.face import Face
+from comgeo.core.vertex import Vertex2D, Vertex3D, Vertex
+
+class TestFace(unittest.TestCase):
+	def setUp(self):
+		self.vertex_ids = [0, 1, 2]
+		self.face = Face(self.vertex_ids, id=5, visited=True, max_num_vertices=4)
+		self.vertices2d = [Vertex2D(0.0, 0.0), Vertex2D(1.0, 0.0), Vertex2D(0.0, 1.0)]
+		self.vertices3d = [Vertex3D(0.0, 0.0, 0.0), Vertex3D(1.0, 0.0, 0.0), Vertex3D(0.0, 1.0, 0.0)]
+
+	def test_init_and_properties(self):
+		self.assertEqual(self.face.id, 5)
+		self.assertTrue(self.face.visited)
+		self.assertEqual(self.face.vertex_ids, self.vertex_ids)
+		self.assertIsNone(self.face._center)
+		self.assertIsNone(self.face._area)
+		self.assertEqual(self.face._max_num_vertices, 4)
+
+	def test_id_setter(self):
+		self.face.id = 10
+		self.assertEqual(self.face.id, 10)
+
+	def test_visited_setter(self):
+		self.face.visited = False
+		self.assertFalse(self.face.visited)
+
+	def test_vertex_ids_setter(self):
+		new_ids = [1, 2, 0]
+		self.face.vertex_ids = new_ids
+		self.assertEqual(self.face.vertex_ids, new_ids)
+
+	def test_vertex_ids_setter_exceeds_max(self):
+		with self.assertRaises(AssertionError):
+			self.face.vertex_ids = [0, 1, 2, 3, 4]
+
+	def test_repr_and_str(self):
+		expected = f"Face(id=5, visited=True, \nvertex_ids={self.vertex_ids})"
+		self.assertEqual(repr(self.face), expected)
+		self.assertEqual(str(self.face), expected)
+
+	def test_get_set_item(self):
+		self.assertEqual(self.face[0], 0)
+		self.face[0] = 9
+		self.assertEqual(self.face[0], 9)
+
+	def test_equality(self):
+		other = Face([0, 1, 2], id=5, visited=True, max_num_vertices=4)
+		self.assertTrue(self.face == other)
+		other2 = Face([0, 1, 3], id=5, visited=True, max_num_vertices=4)
+		self.assertFalse(self.face == other2)
+
+	def test_center_property(self):
+		# Should raise TypeError due to incorrect property usage
+		with self.assertRaises(TypeError):
+			_ = self.face.center(self.vertices2d)
+
+	def test_center_setter(self):
+		from comgeo.core.vertex import Vertex2D
+		v = Vertex2D(0.5, 0.5)
+		self.face.center = v
+		self.assertEqual(self.face._center, v)
+
+	def test_area_property_not_implemented(self):
+		with self.assertRaises(NotImplementedError):
+			self.face.area(self.vertices2d)
+
+	def test_area_setter_not_implemented(self):
+		with self.assertRaises(NotImplementedError):
+			self.face.area = 1.0
+
+	def test_is_convex_not_implemented(self):
+		# Should raise NotImplementedError for Vertex2D
+		with self.assertRaises(NotImplementedError):
+			self.face.is_convex(self.vertices2d)
+
+	def test_is_convex_3d(self):
+		# Should raise NotImplementedError due to is_ccw_3d not implemented
+		face3d = Face([0, 1, 2], max_num_vertices=3)
+		with self.assertRaises(NotImplementedError):
+			face3d.is_convex(self.vertices3d)
+
+	def test_point_cloud_sampling_not_implemented(self):
+		with self.assertRaises(NotImplementedError):
+			self.face.point_cloud_sampling(10, self.vertices2d)
+
+if __name__ == "__main__":
+	unittest.main()
